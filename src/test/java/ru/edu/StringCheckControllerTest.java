6695442685a416b9a65ffb9b40527a12e6e386d9
Aleksandr Fedorov <afedorov@example.com>
2024-01-05 13:00:15 +0300

    refactor tests

--- src/test/java/ru/edu/StringCheckControllerTest.java
+++ src/test/java/ru/edu/StringCheckControllerTest.java
@@ -1,7 +1,55 @@
 package ru.edu;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import ru.edu.controller.BaseController;
+import ru.edu.exception.MyException;
+import ru.edu.service.BaseService;
+
+import java.util.Map;
+
+import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@SpringBootTest
 public class StringCheckControllerTest {
 
+    @Mock
+    private BaseService service;
+
+    @InjectMocks
+    private BaseController controller;
+
+    private final TestData data;
+
+    public StringCheckControllerTest(TestData data) {
+        this.data = data;
+    }
+
+    @Test
+    @Order(1)
+    @DisplayName("Тест пустого контроллера")
+    public void testGet() {
+        ResponseEntity<String> response = controller.getTest();
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals("test", response.getBody());
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("Тест контроллера с вводом")
+    public void testGetInput() throws MyException {
+        when(service.start(data.str)).thenReturn(data.map);
+        ResponseEntity<Map<Character, Integer>> response = controller.getInputTest(data.str);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(data.map, response.getBody());
+    }
 }
---
+++ src/test/java/ru/edu/TestData.java
@@ -0,0 +1,9 @@
+package ru.edu;
+
+import java.util.Map;
+
+public interface TestData {
+
+    String str = "abb";
+    Map<Character, Integer> map = Map.of('a',1, 'b', 2);
+}
