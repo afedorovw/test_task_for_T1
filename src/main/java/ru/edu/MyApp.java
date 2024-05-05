d230ea64faa44881ee5e673ad74bb4a654bc318b
Aleksandr Fedorov <a.fedorovw@yandex.ru>
2024-05-05 19:57:49 +0300

    changed MyApp class

--- src/main/java/ru/edu/MyApp.java
+++ src/main/java/ru/edu/MyApp.java
@@ -1,27 +1,12 @@
 package ru.edu;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.edu.config.Log;
 
 @SpringBootApplication
-public class MyApp implements CommandLineRunner {
-
-    @Value("${server.port}")
-    private String port;
-
-    @Value("${server.address}")
-    private String url;
+public class MyApp {
 
     public static void main(String[] args) {
         SpringApplication.run(MyApp.class, args);
     }
-
-    @Override
-    public void run(String... args) {
-        Log.startLog.info("URL: http://{}:{}", url, port);
-    }
-
 }
